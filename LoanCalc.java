b18412586f0f94a3cd030208edb0dd82ec4b2eb2
sarahmarciano0106 <154500367+sarahmarciano0106@users.noreply.github.com>
2024-01-10 17:10:30 +0200

    Update LoanCalc.java

--- LoanCalc.java
+++ LoanCalc.java
@@ -40,7 +40,12 @@ public class LoanCalc {
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {  
     	// Replace the following statement with your code
-    	return 0;
+		double payment = (loan / n);
+		while(endBalance(loan, rate, n, payment) > 0){
+			payment = payment + epsilon;
+			iterationCounter++;
+		}
+		return payment;
     }
     
     /**
@@ -52,7 +57,20 @@ public class LoanCalc {
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
     	// Replace the following statement with your code
-    	return 0;
+    	iterationCounter = 0;
+		double high = loan;
+		double low = (loan / n);
+		double payment = ((high + low) / 2);
+		while((high - low) > epsilon) {
+			if((endBalance(loan, rate, n, payment)) * (endBalance(loan, rate, n ,low)) > 0) 
+				low = payment;
+			else 
+				high = payment;
+			payment = ((high + low) / 2);
+			iterationCounter++;
+		}
+		return payment;
+			
     }
 	
 	/**
@@ -61,6 +79,11 @@ public class LoanCalc {
 	*/
 	private static double endBalance(double loan, double rate, int n, double payment) {
 		// Replace the following statement with your code
-    	return 0;
-	}
+		double endBal = loan;
+			for(int i = 0; i < n; i++) {
+				endBal = ((endBal - payment) * (1 + (rate / 100)));
+			}		
+			return endBal;
+		}
+		
 }

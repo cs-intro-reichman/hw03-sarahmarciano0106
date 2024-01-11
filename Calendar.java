8b3827ea0cd4f315694629413f1a566d1b36da82
sarahmarciano0106 <154500367+sarahmarciano0106@users.noreply.github.com>
2024-01-11 18:17:30 +0200

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -1,55 +1,95 @@
-/*
- * Checks if a given year is a leap year or a common year,
- * and computes the number of days in a given month and a given year. 
- */
-public class Calendar0 {	
-	
-	// Gets a year (command-line argument), and tests the functions isLeapYear and nDaysInMonth.
-	public static void main(String args[]) {
-		int year = Integer.parseInt(args[0]);
-		isLeapYearTest(year);
-		nDaysInMonthTest(year);
-	}
+/** 
+	 * Prints the calendars of all the years in the 20th century.
+	 */
+	public class Calendar {	
+		// Starting the calendar on 1/1/1900
+		static int dayOfMonth = 1;   
+		static int month = 1;
+		static int year = 1900;
+		static int dayOfWeek = 2;     // 1.1.1900 was a Monday
+		static int nDaysInMonth = 31; // Number of days in January
+		
+		/** 
+		 * Prints the calendars of all the years in the 20th century. Also prints the  
+		 * number of Sundays that occured on the first day of the month during this period.
+		 */
+		public static void main(String args[]) {
+		int whichYear = Integer.parseInt(args[0]);
+
+			// Advances the date and the day-of-the-week from 1/1/1900 till 31/12/1999, inclusive.
+			// Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".
+			// The following variable, used for debugging purposes, counts how many days were advanced so far.
+			int debugDaysCounter = 0; 
+			//// Write the necessary initialization code, and replace the condition
+			//// of the while loop with the necessary condition 
+			while (year < whichYear)
+				advance();
+			while (year == whichYear) {
+				//// Write the body of the while 		
+				if(dayOfWeek == 1){
+					System.out.println(dayOfMonth + "/" + month + "/" + year + " Sunday");
+				}
+				else{
+					System.out.println(dayOfMonth + "/" + month + "/" + year);
+				}
+
+				
+				advance();	 
+				debugDaysCounter++;
+				//// If you want to stop the loop after n days, replace the condition of the
+				//// if statement with the condition (debugDaysCounter == n)
+				if (false) { 
+					break;
+				}
+
+			}
+			//// Write the necessary ending code here
+		 }
+		
+		 // Advances the date (day, month, year) and the day-of-the-week.
+		 // If the month changes, sets the number of days in this month.
+		 // Side effects: changes the static variables dayOfMonth, month, year, dayOfWeek, nDaysInMonth.
+		 private static void advance() {
+			// Replace this comment with your code
+			if(dayOfWeek == 7) dayOfWeek = 1;
+			else dayOfWeek++;
+			if(dayOfMonth == nDaysInMonth(month, year)){
+				if(month == 12) {
+					month = 1;
+					year++;
+					dayOfMonth = 1;
+				}
+				else{
+					month++;
+					dayOfMonth = 1;
+					
+				}
+			}
+			else{
+				dayOfMonth++;
+		 } 
+		 }	 
+		// Returns true if the given year is a leap year, false otherwise.
+		private static boolean isLeapYear(int year) {
+			// Replace the following statement with your code
+			boolean isLeap;
+			isLeap = ((year % 400) == 0);
+			isLeap = isLeap || (((year % 4) == 0) && ((year % 100) != 0));
+			return isLeap;
+		}
 		 
-	// Tests the isLeapYear function.
-	private static void isLeapYearTest(int year) {
-		String commonOrLeap = "common";
-		if (isLeapYear(year)) {
-			commonOrLeap = "leap";
-		}
-		System.out.println(year + " is a " + commonOrLeap + " year");  
-	}
-
-	// Tests the nDaysInMonth function.
-	private static void nDaysInMonthTest(int year) {
-		// Replace this comment with your code
-		for(int i = 1; i <= 12; i++){
-			nDaysInMonth(i, year);
-			System.out.println("Month " + i + " has " + nDaysInMonth(i, year) + " days");
+		// Returns the number of days in the given month and year.
+		// April, June, September, and November have 30 days each.
+		// February has 28 days in a common year, and 29 days in a leap year.
+		// All the other months have 31 days.
+		private static int nDaysInMonth(int month, int year) {
+			// Replace the following statement with your code
+			if(month == 4 || month == 6 || month == 9 || month ==11) return 30;
+			if(month == 1 || month == 3 || month == 4 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) return 31;
+			if(month == 2){
+				if(isLeapYear(year)) return 29;
+				else return 28;
+			}
+			else return 0;
 		}
 	}
-
-	// Returns true if the given year is a leap year, false otherwise.
-	public static boolean isLeapYear(int year) {
-	    // Replace the following statement with your code
-		boolean isLeap;
-		isLeap = ((year % 400) == 0);
-		isLeap = isLeap || (((year % 4) == 0) && ((year % 100) != 0));
-		return isLeap;
-	}
-	 
-	// Returns the number of days in the given month and year.
-	// April, June, September, and November have 30 days each.
-	// February has 28 days in a common year, and 29 days in a leap year.
-	// All the other months have 31 days.
-	public static int nDaysInMonth(int month, int year) {
-		// Replace the following statement with your code
-		if(month == 4 || month == 6 || month == 9 || month ==11) return 30;
-		if(month == 1 || month == 3 || month == 4 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) return 31;
-		if(month == 2){
-			if(isLeapYear(year)) return 29;
-			else return 28;
-		}
-		else return 0;
-	}
-}
